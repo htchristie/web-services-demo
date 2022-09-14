d635eac15fc00aaa8c255f5536fd41244c7a292f
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-14 11:24:00 -0300

    Order entity & ManyToOne and JoinColumn annotations

--- src/main/java/com/udemy/webservicesdemo/entities/User.java
+++ src/main/java/com/udemy/webservicesdemo/entities/User.java
@@ -1,10 +1,9 @@
 package com.udemy.webservicesdemo.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity (name = "tb_user")
@@ -17,6 +16,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -67,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
