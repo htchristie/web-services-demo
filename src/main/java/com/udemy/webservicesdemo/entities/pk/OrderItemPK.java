973e998bbc408e24be536f3ecd5e2be6d8594981
Cristyane Tamioso <christiehespanha@gmail.com>
2022-09-15 11:16:19 -0300

    OrderItem entity, OrderItemPK auxiliary class, Embeddable & EmbeddedId annotations

---
+++ src/main/java/com/udemy/webservicesdemo/entities/pk/OrderItemPK.java
@@ -0,0 +1,53 @@
+package com.udemy.webservicesdemo.entities.pk;
+
+import com.udemy.webservicesdemo.entities.Order;
+import com.udemy.webservicesdemo.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable // declare that a class will be embedded by other entities
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    // no constructor
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK)) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return getOrder().equals(that.getOrder()) && getProduct().equals(that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
